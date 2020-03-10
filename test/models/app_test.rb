2f6ca5c821ad3ed2da26ba161549047bff5f75fe
Yani Rodriguez <yani010712@gmail.com>
2020-03-09 23:43:29 -0400

    Adding assertions

--- test/models/app_test.rb
+++ test/models/app_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AppTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save app without id" do
+    app = App.new name: apps(:app_1).name
+    assert_not app.save, "Saved the app without an id"
+  end
+
+  test "should not save app without name" do
+    app = App.new id: apps(:app_2).id
+    assert_not app.save, "Saved the app without a name"
+  end
+
 end
